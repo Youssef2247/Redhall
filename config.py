07f8736e6e2277ca39f348eacf0350b64903613e
Youssef2247 <youssef.120250422@ejust.edu.eg>
2026-05-13 12:47:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-CUSTOMER_BOT_TOKEN = os.environ.get(8753807533:AAEOnCj-x2-DQyZKvz-eniWadjnAFVls_t4, "")
-STAFF_BOT_TOKEN    = os.environ.get(8504389496:AAGgHZxSdBt9iN8oAO90HpQP-tscors3264, "")
-STAFF_GROUP_ID     = int(os.environ.get(-5227283283, "0"))
+CUSTOMER_BOT_TOKEN = os.environ.get("CUSTOMER_BOT_TOKEN", "")
+STAFF_BOT_TOKEN    = os.environ.get("STAFF_BOT_TOKEN", "")
+STAFF_GROUP_ID     = int(os.environ.get("STAFF_GROUP_ID", "0"))
 DATABASE_PATH      = os.environ.get("DATABASE_PATH", "restaurant.db")
